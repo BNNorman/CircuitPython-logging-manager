386cab2c9b93c8c9fe82e93a5df20e8dcb43e4eb
Brian <brian.n.norman@gmail.com>
2023-07-12 21:03:54 +0100

    fixed a bug which caused an OS error [22]

--- LogManager.py
+++ LogManager.py
@@ -33,7 +33,8 @@ class LogMan:
         assert type(filename) is str,"Expected a string for the filename"
         
         try:
-            LogMan.stream=logging.FileHandler(filename,mode)
+            LogMan.stream=open(filename,mode)
+			LogMan.handler=logging.StreamHandler(stream)
         except Exception as e:
             print(f"Unable to create a file stream {e}")
             raise
@@ -43,7 +44,7 @@ class LogMan:
 	assert LogMan.stream is not None,"LogManager stream must be created first with LogMan.setFileStream(<log file name>)"
         assert level is int and level>=0,"getLogger optional level parameter must be an int>=0"
         log=logging.getLogger(name)
-        log.addHandler(LogMan.stream)
+        log.addHandler(LogMan.handler)
         log.setLevel(level)
         return log
 
